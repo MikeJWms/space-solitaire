de22efb21888e7ecb9ceaf7ed3c4631840855cd5
Michael Williams <michaeljwms@live.com>
2019-07-28 02:13:15 -0700

    created 52 card deck; implemented shuffling

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,20 +1,44 @@
 import React, { useContext } from 'react';
 import DeckContext from '../store/DeckContext';
+import spade from '../assets/spade.svg'
+import diamond from '../assets/diamond.svg'
+import club from '../assets/club.svg'
+import heart from '../assets/heart.svg'
 
 export default function Card(props) {
   const deckState = useContext(DeckContext)
-
   return (
     deckState.cards.map((card, index)=>{
+      let suiteSVG = null;
+      let style = null;
+      switch(card.suite){
+        case 'heart':
+          suiteSVG = heart;
+          style = { color: '#ff0000'};
+          break;
+        case 'diamond':
+            suiteSVG = diamond;
+            style = { color: '#ff0000'};
+          break;
+        case 'club':
+            suiteSVG = club;
+          break;
+        case 'spade':
+            suiteSVG = spade;
+          break;
+      }
+
       return (
         <div className="Card" key={index}>
           <div className="Number">
-            <span>{ card.number }</span>
+            <span style={style}>{ card.displayName }</span>
           </div>
           <div className="suite">
-            <span>{ card.suite }</span>
+            <img src={suiteSVG} alt={card.suite}></img>
           </div>
-          <button onClick={()=>{deckState.incrementNumber(index)}}>Increment</button>
+          <div className="art">
+          </div>
+          {/* <button onClick={()=>{deckState.incrementNumber(index)}}>Increment</button> */}
         </div>
       )
     })
--- src/store/GlobalState.js
+++ src/store/GlobalState.js
@@ -1,29 +1,56 @@
 import React, { useState } from 'react';
-
 import { DeckProvider } from './DeckContext';
 
-const GlobalState = props => {
-  const [cards, setCards] = useState([
-    {
-    number: 2,
-    color: 'black',
-    suite: 'spade'
-    },
-    {
-      number: 3,
-      color: 'black',
-      suite: 'spade'
-    },
-    {
-      number: 4,
-      color: 'black',
-      suite: 'spade'
+
+const assembleDeck = () => {
+  let deck = [];
+    for (let suite of ['spade', 'club', 'heart', 'diamond']) {
+      for (let number = 1; number <= 13; number++) {
+        let displayName = number
+        switch (number) {
+          case 1:
+            displayName = 'A'
+            break;
+          case 11:
+            displayName = 'J'
+            break;
+          case 12:
+            displayName = 'Q'
+            break;
+          case 1:
+            displayName = 'K'
+            break;
+        }
+        deck.push(
+          {
+            number: number,
+            displayName: displayName,
+            suite: suite
+          }
+        )
+      }
     }
-  ])
+    shuffle(deck);
+    return deck;
+}
+
+const shuffle = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+
+
+const GlobalState = props => {
+
+  const [cards, setCards] = useState(assembleDeck);
 
   const incrementNumber = (index) => {
     let newDeck = [...cards]; // copy state
-    newDeck[index] = {...newDeck[index], number: newDeck[index].number += 1};
+    newDeck[index] = { ...newDeck[index], number: newDeck[index].number += 1 };
     setCards(newDeck);
     console.log("incrementing card number", cards);
   }
@@ -33,7 +60,7 @@ const GlobalState = props => {
       cards: cards,
       incrementNumber: incrementNumber
     }}>
-      { props.children }
+      {props.children}
     </DeckProvider>
   )
 }
